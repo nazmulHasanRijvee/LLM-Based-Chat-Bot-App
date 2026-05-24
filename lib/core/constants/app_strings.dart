d93214cbb7c566d8a987bdfc83c1953b468a0057
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-05-24 16:44:38 +0600

    Added Open Router Chat bot API and secured it using .env

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppStrings {
+
+  const AppStrings._();
+
+  static const String appName = 'AI Chat Bot';
+  static const String inputHint = 'Type a message...';
+  static const String imageGenInputHint = 'Describe an image...';
+  static const String emptyChat =
+      'Start a conversation!\nSend a message below.';
+
+  static const String errorNoInternet =
+      'No internet connection. Please check and try again.';
+  static const String errorTimeout = 'Request timed out. Please try again.';
+  static const String errorGeneral = 'Something went wrong. Please try again.';
+
+  // Chat API
+  static final loadKey = dotenv.env['CHAT_API_KEY'] ?? '';
+  static final String apiKey =  'Bearer $loadKey';
+
+  static const String baseUrl = 'https://openrouter.ai/api/v1';
+  static const String model = 'google/gemini-3.1-flash-lite';
+  static const String systemPrompt =
+      'You are a helpful and friendly AI assistant.';
+
+  // Image Generation API
+  static const String imageGenApiKey =
+      '';
+  static const String imageGenBaseUrl = 'https://openrouter.ai/api/v1';
+  static const String imageGenModel = 'google/gemini-2.5-flash-image';
+}
